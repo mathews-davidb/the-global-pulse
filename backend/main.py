2af7d07b3ca26333ea91ee11fe1b375af799b5c5
mathews-davidb <74127124+mathews-davidb@users.noreply.github.com>
2025-06-11 15:37:56 +0000

    added intial setup of api in main and crypto service

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import services.crypto as crypto
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to The Global Pulse API"}
+
+@app.get("/api/crypto")
+def get_crypto():
+    return crypto.get_crypto_data()
---
+++ backend/services/crypto.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_crypto_data():
+    url = "https://api.coingecko.com/api/v3/coins/markets"
+    params = {
+        "vs_currency": "usd",
+        "order": "market_cap_desc",
+        "per_page": 10,
+        "page": 1,
+        "sparkline": False
+    }
+    response = requests.get(url, params=params)
+    return response.json()
